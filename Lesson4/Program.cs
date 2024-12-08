9080a7905aea4b1102004e1af7d85c55292f8a4c
alexdesty <alex.desty@gmail.com>
2024-12-08 17:43:42 +0300

    Add some code

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace Lesson4
+﻿namespace Lesson4task1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var collection = new int[] { 1, 2, 3, 10, 15, 20, 40, 42, 56 };
+
+            Console.WriteLine("Вывод массива при помощи цикла for:");
+            for (var i = 0; i < collection.Length; i++)
+            {
+                Console.Write($"{collection[i]} ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Вывод массива при помощи цикла foreach:");
+            foreach (var i in collection)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Вывод массива при помощи цикла while:");
+            var index = 0;
+            while (index < collection.Length)
+            {
+                Console.Write($"{collection[index]} ");
+                index++;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Вывод массива при помощи цикла do...while:");
+            var index2 = 0;
+            do
+            {
+                Console.Write($"{collection[index2]} ");
+                index2++;
+            }
+            while (index2 < collection.Length);
         }
+
+
     }
 }
---
+++ Lesson4task3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lesson4task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = -20;
+            do
+            {
+                Console.WriteLine($"{i} ");
+                i-=20;
+            }
+            while (i >=-100);
+        }
+    }
+}
